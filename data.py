46ea82e8612947bdd15ea10b86cde3e1a389c57e
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2018-10-24 20:20:06 +0530

    Added function for obtaining all eo ad nasa image urls in a csv.

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
-import requests, json, urllib, urllib.request
+import requests, json, urllib, urllib.request, numpy as np, pandas as pd
+from tqdm import tqdm
 from bs4 import BeautifulSoup
+
 
 def obtain_json():
     parameters={"limit":1000,"days":5000}
-    response=requests.get('https://eonet.sci.gsfc.nasa.gov/api/v2/events?status=closed&source=EO')
+    response=requests.get('https://eonet.sci.gsfc.nasa.gov/api/v2/events?status=closed')
     data = response.json()
 
     output_file = 'json_response.json'
@@ -11,21 +13,90 @@ def obtain_json():
     with open(output_file, 'w') as f:
         json.dump(data, f)
 
+
+def obtain_categories(fpath=None):
+    # with open(fpath, 'r') as f:
+    #     data = json.load(f)
+    #     data = data['events']
+    #
+    # categories = set()
+    # for temp in data:
+    #     for t in temp['categories']:
+    #         categories.add(t['title'])
+    # categories = sorted(categories)
+    categories = ('Drought', 'Dust and Haze', 'Earthquakes', 'Floods', 'Landslides', 'Manmade', 'Sea and Lake Ice', 'Severe Storms', 'Snow', 'Temperature Extremes', 'Volcanoes', 'Water Color', 'Wildfires')
+    return categories
+
+
 def make_soup(url):
-    thepage = urllib.request.urlopen(url)
+    try:
+        thepage = urllib.request.urlopen(url)
+    except urllib.error.HTTPError:
+        return None
+
     soupdata = BeautifulSoup(thepage, "html.parser")
     return soupdata
 
 
-if __name__ == '__main__':
-    soup = make_soup("https://disasters.nasa.gov/hurricane-lane-2018")
+def obtain_image_url(url, id):
+    if id not in ['NASA_DISP', 'EO']:
+        return []
 
+    soup = make_soup(url)
+
+    if soup is None:
+        return []
+
+    image_urls = []
     for img in soup.find_all('img'):
         temp = img.get('src')
         if temp[:1] == '/':
-            image = "https://disasters.nasa.gov" + temp
+            image = None
+            # image = "https://disasters.nasa.gov" + temp
         else:
             image = temp
 
-        if image.split('/')[7] != 'sidebar_thumb':
-            print(image)
+        if id == 'NASA_DISP' and image:
+            if image.split('/')[7] != 'sidebar_thumb':
+                image_urls.append(image)
+
+        elif id == 'EO' and image:
+            image_urls.append(image)
+
+    return image_urls
+
+
+def obtain_url_csv():
+    json_file_path = 'json_response.json'
+    csv_path = 'eo_nasa_urls.csv'
+
+    with open(json_file_path, 'r') as f:
+        data = json.load(f)
+        data = data['events']
+
+    all_cats = obtain_categories()
+    n = len(all_cats)
+    cat_dict = {k: i for i, k in enumerate(all_cats)}
+
+    csv_data = []
+
+    for temp in tqdm(data):
+        y = np.zeros((n,), dtype='int')
+        img_urls = []
+
+        for c in temp['categories']:
+            y[cat_dict[c['title']]] = 1
+
+        for s in temp['sources']:
+            img_urls.extend(obtain_image_url(s['url'], s['id']))
+
+        if img_urls:
+            csv_data.extend([[i] + list(y) for i in img_urls])
+
+    df = pd.DataFrame(csv_data, columns=['url'] + list(all_cats))
+    df.to_csv(csv_path)
+
+
+if __name__ == '__main__':
+    obtain_url_csv()
+
